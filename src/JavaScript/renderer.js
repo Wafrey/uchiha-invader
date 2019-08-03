e2f6b4b8af017d93ef197acb99c3beb73db67e15
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-08-03 12:32:56 +0300

    Backup to stable version

--- src/JavaScript/renderer.js
+++ src/JavaScript/renderer.js
@@ -3,159 +3,168 @@ const gameArea = document.querySelector('.game-area');
 const gameOver = document.querySelector('.game-over');
 const gameScore = document.querySelector('.game-score');
 const gamePoints = document.querySelector('.game-score .points');
-
 gameStart.addEventListener('click', onGameStart);
 document.addEventListener('keydown', onKeyDown);
 document.addEventListener('keyup', onKeyUp);
-
-let state = initialState({
-    areaWidth: gameArea.offsetWidth,
-    fireBallWidth: 40,
-    shurikenWidth: 75,
-    shurikenHeight: 75,
-});
-
+let keys = {};
+let player = {
+    x: 30,
+    y: 200,
+    width: 0,
+    height: 0,
+    lastTimeFiredFireball: 0
+};
+let game = {
+    speed: 3,
+    movingMultiplier: 3,
+    fireBallMultiplier: 3,
+    fireInterval: 1000,
+    cloudSpawnInterval: 7000,
+    shurikenSpawnInterval: 1500,
+    shurikenDestroyBonus: 300
+};
+let gameScene = {
+    isGameActive: true,
+    score: 0,
+    lastCloudSpawn: 0,
+    lastShurikenSpawn: 0
+}
 function onGameStart() {
     gameStart.classList.add('hide');
     alert('game started, click okey to continue! Hint: Use your keyboard, and space to shoot!');
-
     const uchiha = document.createElement('div');
-    uchiha.classList.add('uchiha');
-    uchiha.style.top = state.player.y + 'px';
-    uchiha.style.left = state.player.x + 'px';
+    uchiha.classList.add('uchiha')
+    uchiha.style.top = player.y + 'px';
+    uchiha.style.left = player.x + 'px';
     gameArea.appendChild(uchiha);
-    state.player.width = uchiha.offsetWidth;
-    state.player.height = uchiha.offsetHeight;
-
-    window.requestAnimationFrame(frame(0))
+    player.width = uchiha.offsetWidth;
+    player.height = uchiha.offsetHeight;
+    window.requestAnimationFrame(gameAction)
 }
-
-const frame = t1 => t2 => {
-    if (t2 - t1 > game.frameLength) {
-        state = next(state);
-        gameActionDraw(t2, state);
-        state.gameScene.isGameActive && window.requestAnimationFrame(frame(t2));
-    } else {
-        window.requestAnimationFrame(frame(t1));
-    }
-}
-
-function gameActionDraw(timestamp) {
-
+function gameAction(timestamp) {
     const uchiha = document.querySelector('.uchiha');
-
     // Shuriken
-    if (timestamp - state.gameScene.lastShurikenSpawn > game.shurikenSpawnInterval) {
+    if (timestamp - gameScene.lastShurikenSpawn > game.shurikenSpawnInterval) {
         let shuriken = document.createElement('div');
         shuriken.classList.add('shuriken');
         shuriken.x = gameArea.offsetWidth - 75;
         shuriken.style.left = shuriken.x + 'px';
         shuriken.style.top = (gameArea.offsetHeight - 75) * Math.random() + 'px';
-
         gameArea.appendChild(shuriken);
-        state.gameScene.lastShurikenSpawn = timestamp;
-
-        state.shurikens.push({
-            x: gameArea.offsetWidth - 75,
-            y: (gameArea.offsetHeight - 75) * Math.random() + 'px',
-            el: shuriken
-        });
+        gameScene.lastShurikenSpawn = timestamp;
     }
-
     // Clouds
-    if (timestamp - state.gameScene.lastCloudSpawn > game.cloudSpawnInterval) {
+    if (timestamp - gameScene.lastCloudSpawn > game.cloudSpawnInterval) {
         let cloud = document.createElement('div');
         cloud.classList.add('cloud');
         cloud.x = gameArea.offsetWidth - 300;
         cloud.style.left = cloud.x + 'px';
         cloud.style.top = (gameArea.offsetHeight - 300) * Math.random() + 'px';
-
         gameArea.appendChild(cloud);
-        state.gameScene.lastCloudSpawn = timestamp;
+        gameScene.lastCloudSpawn = timestamp;
     }
-
     // Shuriken modify
     let shurikens = document.querySelectorAll('.shuriken');
     shurikens.forEach(shuriken => {
         shuriken.x -= game.speed * 2;
         shuriken.style.left = shuriken.x + 'px';
-
         if (shuriken.x + shurikens.offsetWidth <= 0) {
             shuriken.parentElement.removeChild(shuriken);
         }
     })
-
     // Clouds modify
     let clouds = document.querySelectorAll('.cloud');
     clouds.forEach(cloud => {
         cloud.x -= game.speed;
         cloud.style.left = cloud.x + 'px';
-
         if (cloud.x + clouds.offsetWidth <= 0) {
             cloud.parentElement.removeChild(cloud);
         }
     })
-
     // Fireballs modify
     let fireBalls = document.querySelectorAll('.fireball');
-
-    state.fireBalls.forEach(f => f.el.style.left = f.x + 'px');
-
-    
-
+    fireBalls.forEach(fireBall => {
+        fireBall.x += game.speed * game.fireBallMultiplier;
+        fireBall.style.left = fireBall.x + 'px';
+        if (fireBall.x + fireBall.offsetWidth > gameArea.offsetWidth) {
+            fireBall.parentElement.removeChild(fireBall);
+        }
+    })
     // Gravity
-    let isInAir = (state.player.y + state.player.height) + 50 < gameArea.offsetHeight;
+    let isInAir = (player.y + player.height) + 50 < gameArea.offsetHeight;
     if (isInAir) {
-        state.player.y += game.speed;
-        uchiha.style.top = state.player.y + 'px';
+        player.y += game.speed;
+        uchiha.style.top = player.y + 'px';
     }
-
     // Register Player input.
-    if (keys.ArrowUp && state.player.y > 35) {
-        state.player.y -= game.speed * game.movingMultiplier;
-        uchiha.style.top = state.player.y + 'px';
+    if (keys.ArrowUp && player.y > 35) {
+        player.y -= game.speed * game.movingMultiplier;
+        uchiha.style.top = player.y + 'px';
     }
-
     if (keys.ArrowDown && isInAir) {
-        state.player.y += game.speed * game.movingMultiplier;
-        uchiha.style.top = state.player.y + 'px';
+        player.y += game.speed * game.movingMultiplier;
+        uchiha.style.top = player.y + 'px';
     }
-
-    if (keys.ArrowLeft && state.player.x > 10) {
-        state.player.x -= game.speed * game.movingMultiplier;
-        uchiha.style.left = state.player.x + 'px';
+    if (keys.ArrowLeft && player.x > 10) {
+        player.x -= game.speed * game.movingMultiplier;
+        uchiha.style.left = player.x + 'px';
     }
-
-    if (keys.ArrowRight && state.player.x + state.player.width + 10 < gameArea.offsetWidth) {
-        state.player.x += game.speed * game.movingMultiplier;
-        uchiha.style.left = state.player.x + 'px';
+    if (keys.ArrowRight && player.x + player.width + 10 < gameArea.offsetWidth) {
+        player.x += game.speed * game.movingMultiplier;
+        uchiha.style.left = player.x + 'px';
     }
-
-    if (keys.Space && timestamp - state.player.lastTimeFiredFireball > game.fireInterval) {
+    if (keys.Space && timestamp - player.lastTimeFiredFireball > game.fireInterval) {
         uchiha.classList.add('uchiha-fireball');
-
         // Fireball
-        addFireBall(state);
-        state.player.lastTimeFiredFireball = timestamp;
+        addFireBall(player);
+        player.lastTimeFiredFireball = timestamp;
     } else {
         uchiha.classList.remove('uchiha-fireball');
     }
-
     // Collision detection
     shurikens.forEach(shuriken => {
         if (isCollision(uchiha, shuriken)) {
             gameOverAction();
         }
-
         fireBalls.forEach(fireBall => {
             if (isCollision(fireBall, shuriken)) {
-                state.gameScene.score += game.shurikenDestroyBonus;
+                gameScene.score+=game.shurikenDestroyBonus;
                 shuriken.parentElement.removeChild(shuriken);
-                //fireBall.parentElement.removeChild(fireBall);
+                fireBall.parentElement.removeChild(fireBall);
             }
         })
     })
-
     // Score
-    gamePoints.textContent = state.gameScene.score++;
+    gamePoints.textContent = gameScene.score++;
+    if (gameScene.isGameActive) {
+        window.requestAnimationFrame(gameAction);
+    }
+}
+function gameOverAction() {
+    gameScene.isGameActive = false;
+    gameScore.parentElement.removeChild(gameScore);
+    gameOver.textContent = `Game Over! You've reached ${gameScene.score} points. \n Refresh the page to play again!`;
+    gameOver.classList.remove('hide');
+}
+function isCollision(firstEl, secondEl) {
+    let firstRect = firstEl.getBoundingClientRect();
+    let secondRect = secondEl.getBoundingClientRect();
+    return !(firstRect.top > secondRect.bottom ||
+        firstRect.bottom < secondRect.top ||
+        firstRect.right < secondRect.left ||
+        firstRect.left > secondRect.right)
+}
+function addFireBall(player) {
+    let fireBall = document.createElement('div');
+    fireBall.classList.add('fireball');
+    fireBall.style.top = (player.y + player.height / 2 - 60) + 'px';
+    fireBall.x = (player.x + player.width);
+    fireBall.style.left = fireBall.x + 'px';
+    gameArea.appendChild(fireBall);
+}
+function onKeyDown(e) {
+    keys[e.code] = true;
+}
+function onKeyUp(e) {
+    keys[e.code] = false;
 }
